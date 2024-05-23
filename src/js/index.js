7b182f6abe8e568c2ec45605b81dfcf11a99eab6
catalinbroinas <catalinbroinas@yahoo.com>
2024-05-23 16:52:05 +0300

    Test `getResponse` method

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import '../css/style.css';
 import { WeatherAPI } from './weatherFetch';
+
+const weather = WeatherAPI();
+weather.setLocation('Oradea');
+weather.getResponse()
+    .then(data => {
+        console.log('Response:', data);
+    }).catch(err => {
+        console.error('Error:', err);
+    });
