a8aa2b8a61d31444f2e58b4b3faab865674d0e84
catalinbroinas <catalinbroinas@yahoo.com>
2024-05-25 15:38:24 +0300

    Add `validateField` method to `FormValidator`

--- js/main.js
+++ js/main.js
@@ -687,7 +687,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   FormValidator: () => (/* binding */ FormValidator)
 /* harmony export */ });
 function FormValidator(formId) {
+    const form = document.querySelector(`#${formId}`);
 
+    const validateField = (fieldId) => {
+        const field = form.querySelector(`#${fieldId}`);
+        const spanError = form.querySelector(`#${fieldId} ~ span.error`);
+
+        if (field.validity.valid) {
+            spanError.textContent = '';
+            spanError.classList.toggle('active');
+        } else {
+            spanError.classList.toggle('active');
+
+            if (field.validity.valueMissing) {
+                spanError.textContent = 'This field is required.';
+            } else if (field.validity.typeMismatch) {
+                spanError.textContent = `This field has been filled in incorrectly. ${field.validationMessage}`;
+            } else if (field.validity.tooShort) {
+                spanError.textContent = `This field has been filled in incorrectly. ${field.validationMessage}`;
+            } else if (field.validity.tooLong) {
+                spanError.textContent = `This field has been filled in incorrectly. ${field.validationMessage}`;
+            } else if (field.validity.patternMismatch) {
+                spanError.textContent = `This field was not filled in correctly. 
+                Please enter a city name. ${field.validationMessage}`;
+            }
+        }
+    };
 }
 
 
